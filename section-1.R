75a187516f0979b7e8c7bff6ff3cd4f97602ff12
Michael Mallari <hello@michaelmallari.com>
2019-02-01 15:18:19 -0500

    Updated notes

--- section-1.R
+++ section-1.R
@@ -1,13 +1,16 @@
-# Data Science: R Basics (HarvardX PH125.1x) | Section 1: R Basics, Functions, and Data Types
+# Data Science: R Basics (HarvardX PH125.1x)
+# Section 1: R Basics, Functions, and Data Types
 
-# Resources:
+# Additional Resources:
 # https://rpubs.com/michaelmallari/harvardx-data-science-section-1
 # http://www.michaelmallari.com/data/
 
 
-# Install and load dslabs package
+# Install and load dslabs package and murders data set
 install.packages("dslabs")
 library(dslabs)
+data("murders")
+
 
 
 a <- 1
@@ -34,3 +37,35 @@ log(2.718282)
 exp(log(9))
 log(exp(3))
 
+
+# Show the structures of the murders data frame
+str(murders)
+# Show the first 6 observations (rows)
+head(murders)
+# Show the last 6 observations
+tail(murders)
+# Show the names of the variables (columns) of the data frame
+names(murders)
+
+
+# Create a numeric vector (pop) of the just the population variable
+pop <- murders$population
+pop
+# Length of pop
+length(pop)
+# Class of pop
+class(pop)
+
+
+# Verify the class of a character vector
+class(murders$state)
+
+
+# Verify the class of a logical variable
+z <- -3 == 2
+z
+class(z)
+
+
+# Verify the class of a factors (categorical variable) vector
+class(murders$region)
